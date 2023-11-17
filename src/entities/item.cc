aba4fefcd1f3f3719edaf205bdc5378be059979f
lsaamaral <isabellaamaral@outlook.pt>
2023-11-17 02:49:34 -0300

    adicionando imagem dos itens

--- src/entities/item.cc
+++ src/entities/item.cc
@@ -5,27 +5,27 @@ Item::Item(){}
 Item::Item(int tipo_item){
   if(tipo_item == 0){ // Item de + Vida
     attributes_.hp = 10;
-    img_item_texture_.loadFromFile("resources/hp_item.png");
+    img_item_texture_.loadFromFile("resources/itens/hp_item.png");
     img_item_.setTexture(img_item_texture_);
   }else if(tipo_item == 1){ // Item de + Ataque
     attributes_.atk = 5;
-    img_item_texture_.loadFromFile("resources/atk_item.png");
+    img_item_texture_.loadFromFile("resources/itens/atk_item.png");
     img_item_.setTexture(img_item_texture_);
   }else if(tipo_item == 2){ // Item de + Defesa
     attributes_.def = 5;
-    img_item_texture_.loadFromFile("resources/def_item.png");
+    img_item_texture_.loadFromFile("resources/itens/def_item.png");
     img_item_.setTexture(img_item_texture_);
   }else if(tipo_item == 3){ // Item de + Energia
     attributes_.mp = 10;
-    img_item_texture_.loadFromFile("resources/mp_item.png");
+    img_item_texture_.loadFromFile("resources/itens/mp_item.png");
     img_item_.setTexture(img_item_texture_);
   }else if(tipo_item == 4){ // Item de + Experiencia
     attributes_.xp = 5;
-    img_item_texture_.loadFromFile("resources/xp_item.png");
+    img_item_texture_.loadFromFile("resources/itens/xp_item.png");
     img_item_.setTexture(img_item_texture_);
   }else if(tipo_item == 5){ // Item de + Esquiva
     attributes_.agi = 5;
-    img_item_texture_.loadFromFile("reosurces/agi_item.png");
+    img_item_texture_.loadFromFile("reosurces/itens/agi_item.png");
     img_item_.setTexture(img_item_texture_);
   }
 }
