eeb7c7b89372f651e6b2a58561a9ca6172541fb0
lsaamaral <isabellaamaral@outlook.pt>
2023-11-25 01:23:28 -0300

    uau uma skill que da certo (eu acho)

--- src/entities/skill.cc
+++ src/entities/skill.cc
@@ -1,38 +1,48 @@
 #include "../../include/skill.h"
-#include "../../include/player.h"
-#include "../../include/enemy.h"
 
-Skill::Skill(int classe, int index, Player* player, Enemy* enemy) 
-    : player_ref(player), enemy_ref(enemy) {
-    // Referência utilizada para mudar os atributos de jogador e inimigo
-    if(classe == 0){ // Knight
-        if(index == 0){ // Guerreiro de Aço
-            player_ref->AumentaHp(10);
-        }else if(index == 1){ // Quebra-Escudo
-            player_ref->AumentaAgi(10, 3);
-            enemy_ref->ReduzHp(10);
-        }else if(index == 2){ // Golpe Devastador
-            enemy_ref->ReduzHp(20);
+Skill::Skill(int classe, int index) {
+    if (classe == 0) {  // Knight
+        if (index == 0) {
+            attributes_.hp = 20;  // Aumenta HP do jogador
+            img_skill_texture_.loadFromFile("resources/knight/knight_hp.png");
+        } else if (index == 1) {
+            attributes_.agi = 10;  // Aumenta a agilidade do jogador
+            attributes_.hp = -15;  // Diminui a vida do inimigo 
+            img_skill_texture_.loadFromFile("resources/knight/Run+Attack.png");
+        } else if (index == 2) {
+            attributes_.hp = -30;  // Diminui mais que na segunda skill
+            img_skill_texture_.loadFromFile("resources/knight/Attack_2.png"); //a ideia é que seja uma sprite do attack1 + attack2
         }
-    } else if(classe == 1){ // Mage
-        if(index == 0){ // Barreira da Luz
-            player_ref->AumentaDef(10, 3);
-        }else if(index == 1){ // Raio Singular
-            enemy_ref->ReduzHp(20);
-        }else if(index == 2){ // Arco Místico
-            enemy_ref->ReduzHp(30);
+    } else if (classe == 1) {  // Mage
+        if (index == 0) {
+            attributes_.def = 10;  // Aumenta a defesa do jogador
+            img_skill_texture_.loadFromFile("resources/mage/Attack_1.png");
+        } else if (index == 1) {
+            attributes_.hp = -15;  // Diminui a vida do inimigo
+            img_skill_texture_.loadFromFile("resources/mage/Attack_2.png"); //é pra ser com a Charge 1
+        } else if (index == 2) {
+            attributes_.hp = -20;  // Diminui a vida do inimigo mais que na segunda skill
+            img_skill_texture_.loadFromFile("resources/mage/Attack_2.png"); // é pra ser com a Charge 2
         }
-    } else if(classe == 2){ // Samurai
-        if(index == 0){ // Coração da Batalha
-            player_ref->AumentaMp(15);
-        }else if(index == 1){ // Espada Fantasma
-            enemy_ref->ReduzHp(20);
-        }else if(index == 2){ // Falhas da Armadura
-            enemy_ref->ReduzDef(15, 3);
+    } else if (classe == 2) {  // Samurai
+        if (index == 0) {
+            attributes_.mp = 15;  // Aumenta o MP do jogador
+            img_skill_texture_.loadFromFile("resources/samurai/samurai_mana.png");
+        } else if (index == 1) {
+            attributes_.hp = -20;  // Diminui a vida do inimigo
+            img_skill_texture_.loadFromFile("resources/samurai/Attack_1.png");
+        } else if (index == 2) {
+            attributes_.def = -10;  // Diminui a defesa do inimigo
+            img_skill_texture_.loadFromFile("resources/samurai/Attack_2.png"); //a ideia é que seja attack 2 + attack 3
         }
-    } else if(classe == 4){ // Boss
-
+    } else if (classe == 4) { // Boss
+        if (index == 0){
+            attributes_.hp = -50;  // Diminui a vida do jogador consideravelmente
+            img_skill_texture_.loadFromFile("resources/boss/skill_boss.png"); //nao temos ainda
+        }
     }
+
+    img_skill_.setTexture(img_skill_texture_);
 }
 
-Skill::Skill(){}
+Skill::Skill() {}
